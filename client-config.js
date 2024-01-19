9b361badf6f1644ad6d59558a2164bfc771c99f4
Jaybee Montejo <montejo.jaybee@gmail.com>
2024-01-19 08:48:09 +0800

    setup configs to get started

---
+++ client-config.js
@@ -0,0 +1,8 @@
+const clientConfig = {
+	siteUrl: 'https://bisogo.ph', // e.g. 'https://xyz.com'
+    graphqlUrl: 'https://bisogo.ph/graphql' // e.g. 'https://wordpressSiteUrl.com/graphql',
+    productImagePlaceholder: 'https://via.placeholder.com/800', // e.g https://via.placeholder.com/434 - Placeholder image URL for index page
+    singleImagePlaceholder: 'https://via.placeholder.com/200', // e.g https://via.placeholder.com/200 - Placeholder image URL for individual product page
+};
+
+export default clientConfig;
---
+++ wooConfig.js
@@ -0,0 +1,6 @@
+const wooConfig = {
+	siteUrl: 'https://bisogo.ph', // e.g. 'https://xyz.com'
+    graphqlUrl: 'https://bisogo.ph/graphql' // e.g. 'https://wordpressSiteUrl.com/graphql'
+};
+
+module.exports = wooConfig;
